d60396d6d329d9348acbb354cc48cf3daa70fd31
James Seppi <james.seppi@gmail.com>
2015-05-20 10:48:53 -0500

    starting on redo of projects list

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+L.mapbox.accessToken = 'pk.eyJ1IjoianNlcHBpIiwiYSI6ImFVcnNCTUUifQ.GLIvMEJCaM1402zRo55ITw';
+// var map = L.mapbox.map('left-sidebar', 'jseppi.ipgbh1ko', {
+//   zoom: 14,
+//   center: [30.281787488776338, -97.74355888366699],
+//   dragging: false,
+//   touchZoom: false,
+//   scrollWheelZoom: false,
+//   boxZoom: false,
+//   zoomControl: false
+// });
