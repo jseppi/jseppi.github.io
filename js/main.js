41f354a8d3167f0621cf66cf7fdca0a998c35981
James Seppi <james.seppi@gmail.com>
2015-10-29 16:52:24 -0500

    add popular repo list

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/* global m */
+(function () {
+ 
+  var Repo = {
+    list: function() {
+      return m.request({
+        method: "GET",
+        url: "https://api.github.com/users/jseppi/repos?sort=pushed"
+      }).then(function (repos) {
+        return repos.filter(function (repo) {
+          return !repo.fork && repo.stargazers_count > 1;
+        }).sort(function (a, b) {
+          return b.stargazers_count - a.stargazers_count;
+        });
+      });
+    }
+  };
+
+  var PopularRepos = {
+    controller: function () {
+      var repos = Repo.list();
+      return {
+        repos: repos
+      };
+    },
+
+    view: function (ctrl) {
+      return m("ul", [
+        ctrl.repos().map(function (repo) {
+          return m("li", m("a", {href: repo.html_url}, repo.name));
+        }),
+      ]);
+    }
+  };
+
+  m.mount(document.getElementById("repo-list"), PopularRepos);
+})();
